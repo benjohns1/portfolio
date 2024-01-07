7a0dc0fe5cc6ea7a6b021359ed153a5455d67096
Ben Johns <benjohns.git.gpg.donotreply@gmail.com>
2024-01-07 17:54:50 -0600

    add rss links, remove generated feed from git

--- layouts/Baseof.js
+++ layouts/Baseof.js
@@ -18,19 +18,32 @@ const Base = ({
   const { meta_image, meta_author, meta_description } = config.metadata;
   const { base_url } = config.site;
   const router = useRouter();
-
+  const pageTitle = plainify( meta_title ? meta_title : title ? title : config.site.title);
+  const pageDescription = plainify(description ? description : meta_description);
   return (
     <>
       <Head>
-        {/* title */}
-        <title>
-          {plainify(
-            meta_title ? meta_title : title ? title : config.site.title
-          )}
-        </title>
+        <title>{pageTitle}</title>
 
         {/* canonical url */}
         {canonical && <link rel="canonical" href={canonical} itemProp="url" />}
+
+        {/* RSS links */}
+        <link
+          rel="alternate"
+          type="application/atom+xml"
+          title={`RSS Atom Feed for ${pageTitle}`}
+          href="/feed/rss.xml" />
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title={`RSS Feed for ${pageTitle}`}
+          href="/feed/rss.xml" />
+        <link
+          rel="alternate"
+          type="application/json"
+          title={`JSON Feed for ${pageTitle}`}
+          href="/feed/feed.json" />
 
         {/* noindex robots */}
         {noindex && <meta name="robots" content="noindex,nofollow" />}
@@ -38,7 +51,7 @@ const Base = ({
         {/* meta-description */}
         <meta
           name="description"
-          content={plainify(description ? description : meta_description)}
+          content={pageDescription}
         />
 
         {/* author from config.json */}
@@ -47,15 +60,13 @@ const Base = ({
         {/* og-title */}
         <meta
           property="og:title"
-          content={plainify(
-            meta_title ? meta_title : title ? title : config.site.title
-          )}
+          content={pageTitle}
         />
 
         {/* og-description */}
         <meta
           property="og:description"
-          content={plainify(description ? description : meta_description)}
+          content={pageDescription}
         />
         <meta property="og:type" content="website" />
         <meta
@@ -66,15 +77,13 @@ const Base = ({
         {/* twitter-title */}
         <meta
           name="twitter:title"
-          content={plainify(
-            meta_title ? meta_title : title ? title : config.site.title
-          )}
+          content={pageTitle}
         />
 
         {/* twitter-description */}
         <meta
           name="twitter:description"
-          content={plainify(description ? description : meta_description)}
+          content={pageDescription}
         />
 
         {/* og-image */}
